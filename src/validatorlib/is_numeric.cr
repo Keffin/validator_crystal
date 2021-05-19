0f8e65c68746a09c8a45ec19508dfac07518f7d4
Kevin Alemi <kevin.alemi97@gmail.com>
2021-05-19 17:56:51 +0200

    is numeric

--- src/validatorlib/is_numeric.cr
+++ src/validatorlib/is_numeric.cr
@@ -1,5 +1,14 @@
 module ValidatorCrystal
 
+    pattern = /^(-)*(.)*[0-9]+(.)*$/
     
+    def self.is_numeric(input : String, pattern : Regex) : Bool 
+        !input.empty? && !input.blank? && input.match(pattern) ? true : false 
+    end 
 
+    puts is_numeric("42", pattern)
+    puts is_numeric("22.33", pattern)
+    puts is_numeric("0", pattern)
+    puts is_numeric("-42", pattern)
+    
 end 
